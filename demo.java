77341ce64c1dfd0ac283d34bb9abd825c303836a
ZapPower <1kindlekid@gmail.com>
2024-05-10 22:19:43 -0400

    reduced demo size for time purposes

--- demo.java
+++ demo.java
@@ -1,11 +1,9 @@
 public class demo {
     public static void main(String[] args) {
         monster myMonster = new monster("Jerry", 20);
-        myMonster.heal(10);
-        while (!myMonster.isDefeated()) {
-            System.out.println(myMonster.getName() + " has " + myMonster.getHealth() + " health!");
-            myMonster.takeDamage(10);
-        }
-        myMonster.heal(5);
+        monster myMonster2 = new monster("John", 10);
+        System.out.println(myMonster.getName() + " has " + myMonster.getHealth() + " health!");
+        myMonster2.heal(10);
+        System.out.println(myMonster2.getName() + " has " + myMonster2.getHealth() + " health!");
     }
 }
--- monster.java
+++ monster.java
@@ -1,28 +1,14 @@
 public class monster {
     private int health;
     private String name;
-    private boolean defeated;
 
     public monster(String name, int health) {
         this.health = health;
         this.name = name;
-        defeated = false;
     }
 
     public void takeDamage(int amount) {
         this.health -= amount;
-        if (health <= 0) {
-            System.out.println(this.name + " was defeated!");
-            this.defeated = true;
-        }
-    }
-
-    public void heal(int amount) {
-        if (health <= 0) {
-            System.out.println(this.name + " was already defeated!");
-            return;
-        }
-        this.health += amount;
     }
 
     public int getHealth() {
@@ -31,9 +17,5 @@ public class monster {
 
     public String getName() {
         return this.name;
-    }
-
-    public boolean isDefeated() {
-        return this.defeated;
     }
 }
